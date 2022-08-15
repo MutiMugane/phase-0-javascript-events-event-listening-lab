75bcb66104d5e15c17e0883baac65386472cfaad
+ <+>
2022-08-15 10:07:57 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-function addingEventListener() {
+addEventListener(clickAlert,addEventListener);{
+const input = document.getElementById('button');
+input.clickAlert()('click', function() {
+  alert('I was clicked!');
+});
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener(clickAlert,addEventListener)('click', clickAlert);
+
+}
+
+function addingEventListener(){
+
+  const input = document.getElementById('button');
+  function clickAlert(){
+    alert('I was clicked')
+
+  }
+input.addEventListener('click',clickAlert)
 }
